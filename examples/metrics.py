2de50045efbba08e3e377a9cc531ef190a6332c3
Wenzhao Qiu <qwzhao@umich.edu>
2023-01-20 13:52:06 -0500

    fixed similarity metric

--- examples/metrics.py
+++ examples/metrics.py
@@ -6,6 +6,8 @@ SENTIMENT = 2
 OPINION = 3
 I = 4
 
+NUM_QUAD_ELTS = 5
+
 SIMILARITY_THRESHOLD = 0.50
 
 
@@ -55,14 +57,31 @@ def indexify_opinions(annotation, review):
 
 def flatten_quad(quad):
     opinion = quad.pop(OPINION)
-    quad.append([idx for idx in opinion])
+    quad.extend([idx for idx in opinion])
 
 
 def get_quad_similarity(quad1, quad2):
-    set_flat_quad1 = set(tuple(flatten_quad(list(quad1))))
-    set_flat_quad2 = set(tuple(flatten_quad(list(quad2))))
-    inter, union = get_inter_union(set_flat_quad1, set_flat_quad2)
-    return inter/union
+    # set_flat_quad1 = set(quad1)
+    # set_flat_quad2 = set(quad2)
+    # inter, union = get_inter_union(set_flat_quad1, set_flat_quad2)
+    # return inter/union
+    list_quad1 = list(quad1)
+    list_quad2 = list(quad2)
+    opinion1 = list_quad1.pop(OPINION)
+    opinion2 = list_quad2.pop(OPINION)
+    aspect1 = list_quad1.pop(ASPECT)
+    aspect2 = list_quad2.pop(ASPECT)
+
+    # inter, union = get_inter_union(set(list_quad1), set(list_quad2))
+    num_agreements = [elt[0] == elt[1] for elt in zip(list_quad1, list_quad2)]
+    inter_opinion, union_opinion = get_inter_union(
+        set(opinion1), set(opinion2))
+    inter_aspect, union_aspect = get_inter_union(set(aspect1), set(aspect2))
+    sum_agreements = sum(num_agreements)
+    inter_union_opinion = inter_opinion/union_opinion
+    inter_union_aspect = inter_aspect/union_aspect
+
+    return (sum_agreements + inter_union_opinion + inter_union_aspect)/NUM_QUAD_ELTS
 
 
 f = open("example_format.json")
@@ -78,6 +97,8 @@ for idx in range(len(review_data)):
     indexify_opinions(annotation1, review)
     indexify_opinions(annotation2, review)
 
+    # TODO: indexify aspects
+
     # Delta
     print(f"Delta: {abs(len(annotation1) - len(annotation2))}")
 
@@ -91,6 +112,7 @@ for idx in range(len(review_data)):
     print_metric("Exact match", exact_inter, exact_union)
 
     # Aspect
+    # TODO: make into spans
     get_quad_elt_metric(ASPECT, "Aspect")
 
     # Category
@@ -122,14 +144,17 @@ for idx in range(len(review_data)):
 
     for i in range(len(dif1)):
         for j in range(len(dif2)):
-            if i != j and ({dif1[i], dif2[j]} not in set_list):
+            if {dif1[i], dif2[j]} not in set_list:
                 similarity = get_quad_similarity(dif1[i], dif2[j])
                 if similarity > SIMILARITY_THRESHOLD:
                     set_list.append({dif1[i], dif2[j]})
+                    # What is the numerator / denominator?
                     num += similarity
                     denom -= similarity
 
     print(f"Adjusted match: {num}/{denom}, {(num/denom)*100}%")
+
+    # TODO: Exact match when take away each column
 
     print()
 

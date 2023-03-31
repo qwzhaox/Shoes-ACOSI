a12007f7c8dba36fbb5b17bde2f885961154032a
Wenzhao Qiu <qwzhao@umich.edu>
2023-03-31 02:15:27 -0400

    api

--- chatGPT/chat.py
+++ chatGPT/chat.py
@@ -1,9 +1,11 @@
+import os
+
 import openai
 from dotenv import load_dotenv
 
 load_dotenv()
 
-openai.api_key =
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 response = openai.Completion.create(
     model="gpt-3.5-turbo",

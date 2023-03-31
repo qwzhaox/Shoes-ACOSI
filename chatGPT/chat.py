d9b1074317c6e4f35481db92c3de23c3685830de
Wenzhao Qiu <qwzhao@umich.edu>
2023-03-31 02:25:04 -0400

    a

--- chatGPT/chat.py
+++ chatGPT/chat.py
@@ -1,15 +1,25 @@
 import os
 
 import openai
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)  # for exponential backoff
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-response = openai.Completion.create(
-    model="gpt-3.5-turbo",
-    prompt="say hello"
-)
+
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+def completion_with_backoff(**kwargs):
+    return openai.Completion.create(**kwargs)
+
+
+response = completion_with_backoff(
+    model="gpt-3.5-turbo", prompt="say \"Hello World\"")
 
 print(response)

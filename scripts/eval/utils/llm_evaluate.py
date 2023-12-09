c588515e1fd9b1bd0c9858f175091778848ef11c
qwzhaox <qwzhao@umich.edu>
2023-12-09 15:45:03 -0500

    oops

--- scripts/eval/utils/llm_evaluate.py
+++ scripts/eval/utils/llm_evaluate.py
@@ -5,4 +5,9 @@ def get_llm_output(pkl_file):
     with open(pkl_file, "rb") as file:
         llm_outputs = load(file)
 
+    for output in llm_outputs:
+       for i, quint in enumerate(output):
+            quint = tuple([x.lower() for x in quint])
+            output[i] = quint
+
     return llm_outputs
